34efdb07903a7f2f687badb66c959d46f94033fc
farhanmdrmdn <73457012+farhanmdrmdn@users.noreply.github.com>
2020-12-14 13:56:39 +0800

    updating cart function

--- finalproject/assets/main.js
+++ finalproject/assets/main.js
@@ -1,17 +1,83 @@
 let carts = document.querySelectorAll('.add-cart');
 
 let products = [
-    {
+{
         name: "Asher Tall Shelf",
-        tag: "ashertallshelf",
+        tag: "asher_tallshelf1.jpg",
         price: 150,
+        inCart: 0
+},
+{
+        name: "Axel Side Chair",
+        tag: "axel_seide_chair1.jpg",
+        price: 100,
+        inCart: 0
+},
+{
+        name: "Bruno Dining Table",
+        tag: "bruno_diningtable1.jpg",
+        price: 1040,
+        inCart: 0
+},
+{
+        name: "Claire Dinner Plates Set",
+        tag: "clare_dinnerplateset1.jpg",
+        price: 54.8,
+        inCart: 0
+},
+{
+        name: "Dual Tone 3 Seater Sofa",
+        tag: "dualtone_3seatersofa1.jpg",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "emelie_tvconsole1.jpg",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "grasshoppa_floorlamp1.jpg",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "isabella_floorlamp1.jpg",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "linate_coffeetable1.jpg",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "reagan_bookshelf1.jpg",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "",
+        price: 0,
+        inCart: 0
+},
+{
+        name: "",
+        tag: "",
+        price: 0,
         inCart: 0
 },
 ]
 
 for (let i = 0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        cartNumbers();
+        cartNumbers(products[i]);
     })
 }
 
@@ -23,7 +89,8 @@ function onLoadCartNumbers() {
     }
 }
 
-function cartNumbers() {
+function cartNumbers(product) {
+    console.log("The product clicked is", product);
     let productNumbers = localStorage.getItem('cartNumbers');
 
     productNumbers = parseInt(productNumbers);
