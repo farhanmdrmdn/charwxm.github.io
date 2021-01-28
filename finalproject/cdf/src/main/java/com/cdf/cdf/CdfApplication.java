091f6ef7b40acfa15a860579b9a7f82cc4c93703
charwxm <73457689+charwxm@users.noreply.github.com>
2021-01-28 12:49:30 +0800

    springboot file added

---
+++ finalproject/cdf/src/main/java/com/cdf/cdf/CdfApplication.java
@@ -0,0 +1,13 @@
+package com.cdf.cdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdfApplication.class, args);
+	}
+
+}
---
+++ finalproject/cdf/src/test/java/com/cdf/cdf/CdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdf.cdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
