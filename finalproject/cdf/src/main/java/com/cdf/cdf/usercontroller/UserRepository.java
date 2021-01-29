3747e0bda74138767726e4de28c53158f73395c3
Farhan Ramdan <farhancsrmdn@gmail.com>
2021-01-29 11:05:55 +0800

    Springboot products

--- finalproject/cdf/src/main/java/com/cdf/cdf/usercontroller/UserRepository.java
+++ finalproject/cdf/src/main/java/com/cdf/cdf/usercontroller/UserRepository.java
@@ -2,11 +2,9 @@ package com.cdf.cdf;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.cdf.cdf.Users;
-
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<Users, Integer> {
+public interface UserRepository extends CrudRepository<com.cdf.cdf.Users, Integer> {
 
 }
